adbf604ec62625455f35b53e5bca2c75a08ae861
millc9988 <millc9988@HRH-IC0027327.wrdsb.ca>
2018-01-08 09:56:32 -0500

    Setting up player

---
+++ core/src/com/soundclout/tycoon/Player.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.soundclout.tycoon;
+
+/**
+ *
+ * @author millc9988
+ */
+public class Player {
+ private int money;
+ private int xPos;
+ private int yPos;
+ private String name;
+ private int fame;
+
+}
