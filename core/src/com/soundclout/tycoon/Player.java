3a114c0d23fa732fb63874782ccca96087e3ffff
millc9988 <millc9988@HRH-IC0027327.wrdsb.ca>
2018-01-09 09:37:56 -0500

    Setting up player

--- core/src/com/soundclout/tycoon/Player.java
+++ core/src/com/soundclout/tycoon/Player.java
@@ -14,5 +14,61 @@ public class Player {
  private int yPos;
  private String name;
  private int fame;
+ 
+ public Player(String name){
+     this.name = name;
+     this.money = 50;
+     this.fame = 0;
+     this.xPos = 0;
+     this.yPos = 0;
+ }
+//getters
+ public int getMoney(){
+     return this.money;
+ }
+ 
+ private int getFame(){
+     return this.fame;
+ }
+ 
+ public int getxPos(){
+     return this.xPos;
+ }
+ 
+ public int getyPos(){
+     return this.yPos;
+ }
+ 
+ public String getName(){
+     return this.name;
+ }
+ 
+
+
+
+//logic stuff
+public void earnMoney(int money){
+    this.money+=money;
+}
+
+public void spendMoney(int money){
+    this.money-=money;
+}
+
+public void move(int xMove,int yMove){
+    if (xMove==2) {
+        this.xPos++;
+    }else if (xMove==0) {
+        this.xPos--;
+    }else if(xMove ==1){   
+    }
+    if (yMove==2) {
+        this.yPos++;
+    }else if (yMove==0) {
+        this.yPos--;
+    }else if(yMove ==1){   
+    }
+}
+
 
 }
