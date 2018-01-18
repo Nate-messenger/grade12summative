569d90e7e466c9a3dad5b3393e57414563efd296
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2018-01-18 10:16:01 -0500

    jan 18

--- core/src/com/soundclout/tycoon/MyTycoonGame.java
+++ core/src/com/soundclout/tycoon/MyTycoonGame.java
@@ -2,9 +2,11 @@ package com.soundclout.tycoon;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyTycoonGame extends ApplicationAdapter {
@@ -35,6 +37,16 @@ public class MyTycoonGame extends ApplicationAdapter {
 
     @Override
     public void render() {
+        BitmapFont font = new BitmapFont();
+        if((p1.getxPos() >= 580 && p1.getxPos() <= 650) && (p1.getyPos() >= 90 && p1.getyPos() <= 110)){
+            font.draw(batch, "DO U WANT TO BUY A SONG?", WIDTH/2, HEIGHT/2);
+            if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+               
+                font.draw(batch, "Want do you want your name to be called", WIDTH/2, HEIGHT/2);
+                
+                Song song = new Song(name);
+            }
+        }
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
